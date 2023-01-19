dfeb04f856f72429b1fc5a8e345e5b49bd653e78
Serena-Dong <serenadong3159@icloud.com>
2023-01-19 16:37:44 +0100

    stampa in pagina

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
-<script setup>
+<script>
+export default {
+    data() {
+        return {
+            title: 'La mia prima app con Vite!'
+        }
+    }
+}
 </script>
 
 <template>
+    <h1>{{ title }}</h1>
 </template>
 
 <style scoped>
+h1 {
+    color: green;
+}
 </style>
